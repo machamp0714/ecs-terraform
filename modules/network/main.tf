50fd964e3593d41ca680a14dfdae2d2001e394b3
machamp0714 <rateliasudaisuki@gmail.com>
2021-05-16 16:15:40 +0900

    add: Public Subnets

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -6,3 +6,23 @@ resource "aws_vpc" "this" {
     map("Name", "${var.tags["system"]}-${var.tags["env"]}-vpc")
   )
 }
+
+resource "aws_subnet" "public_1a" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+  tags = merge(
+    map("Name", "${var.tags["system"]}-${var.tags["env"]}-public-1a")
+  )
+}
+
+resource "aws_subnet" "public_1c" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = true
+  tags = merge(
+    map("Name", "${var.tags["system"]}-${var.tags["env"]}-public-1c")
+  )
+}
