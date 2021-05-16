fab8e383e2646e413af566935958fef2dbfdd22f
machamp0714 <rateliasudaisuki@gmail.com>
2021-05-16 16:29:13 +0900

    refactor: Subnet Number

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,5 +1,13 @@
+locals {
+  vpc_cidr       = "10.0.0.0/16"
+  public_subnets = {
+    "ap-northeast-1a" = 1
+    "ap-northeast-1c" = 2
+  }
+}
+
 resource "aws_vpc" "this" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = local.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = merge(
@@ -7,22 +15,13 @@ resource "aws_vpc" "this" {
   )
 }
 
-resource "aws_subnet" "public_1a" {
+resource "aws_subnet" "public" {
+  for_each                = local.public_subnets
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-northeast-1a"
+  cidr_block              = cidrsubnet(aws_vpc.this.cidr_block, 8, each.value)
+  availability_zone       = each.key
   map_public_ip_on_launch = true
   tags = merge(
-    map("Name", "${var.tags["system"]}-${var.tags["env"]}-public-1a")
-  )
-}
-
-resource "aws_subnet" "public_1c" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "ap-northeast-1c"
-  map_public_ip_on_launch = true
-  tags = merge(
-    map("Name", "${var.tags["system"]}-${var.tags["env"]}-public-1c")
+    map("Name", "${var.tags["system"]}-${var.tags["env"]}-public-${each.key}")
   )
 }
