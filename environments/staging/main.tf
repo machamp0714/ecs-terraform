138c2dc0f0bf94e33b3b64e1df92375ab9eb8af9
machamp0714 <rateliasudaisuki@gmail.com>
2021-05-16 23:14:04 +0900

    add: VPC ountputs

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -9,3 +9,15 @@ module "network" {
     system = "machamp"
   }
 }
+
+module "http-sg" {
+  source = "../../modules/security_group"
+  vpc_id = module.network.vpc_id
+  name   = "http-sg"
+  port   = 80
+  cidr_blocks = ["0.0.0.0/0"]
+  tags        = {
+    env    = "staging"
+    system = "machamp"
+  }
+}
