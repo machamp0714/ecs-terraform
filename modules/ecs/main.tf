bd81436dcd1a6bf53b1feb4021e065f68f1c5601
machamp0714 <rateliasudaisuki@gmail.com>
2021-06-05 23:28:29 +0900

    modify: container port

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -20,7 +20,7 @@ resource "aws_ecs_service" "service" {
   load_balancer {
     target_group_arn = var.lb_target_group_arn
     container_name   = "app"
-    container_port   = 80
+    container_port   = 3000
   }
 
   lifecycle {
